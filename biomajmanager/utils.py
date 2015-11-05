27012ba02eee262e3d5aecdfd12f512ce4071026
horkko <horkko@gmail.com>
2015-11-05 08:29:12 +0100

    First release of utils package

--- biomajmanager/utils.py
+++ biomajmanager/utils.py
@@ -31,6 +31,66 @@ class Utils(object):
         sys.exit(1)
 
     @staticmethod
+    def get_files(path=None):
+        """
+        Return the list of file(s) found for a given path
+        :param path: Path to search from
+        :type path: String
+        :return: List of file(s) found
+        :rtype: List
+        """
+        if not path or not os.path.isdir(path):
+            Utils.error("Path not found: %s" % path)
+        return os.listdir(path)
+
+    @staticmethod
+    def get_deepest_dirs(path=None, full=False):
+        """
+        Get the last directories from a path
+        :param path: Path to start from
+        :type path: String
+        :return: List of directories
+        :rtype: List
+        """
+        if not os.path.exists(path) and os.path.isdir(path):
+            Utils.error("%s does not exists" % path)
+
+        dirs = []
+        for dirpath, dirnames, filenames in os.walk(path):
+            if len(dirnames) == 0:
+                if full:
+                    dirs.append(dirpath)
+                else:
+                    dirs.append(os.path.dirname(dirpath))
+        return dirs
+
+    @staticmethod
+    def get_deepest_dir(path=None, full=False):
+        """
+        Return only one deepest dir from the path
+        :param path: Path
+        :type path: String
+        :param full: Return complete path
+        :type full: Boolean
+        :return: Director name
+        :rtype: String
+        """
+        dirs = Utils.get_deepest_dirs(path, full=full)
+        if len(dirs) > 1:
+            Utils.warn("More than one deepest dir found at %s: Only first returned" % path)
+        return dirs[0]
+
+    @staticmethod
+    def ok(msg):
+        """
+        Print a [OK] msg
+        :param msg: Message to print
+        :type msg: String
+        :return:
+        """
+        print("[OK] %s" % msg)
+
+    @staticmethod
     def reset_timer():
         '''
 
@@ -74,6 +134,11 @@ class Utils(object):
         :rtype: String
         '''
         return os.getenv('USER')
+
+    #@staticmethod
+    #def verbose(msg):
+    #    if Manager.verbose:
+    #        print('[VERBOSE] %s' % msg, file=sys.stdout)
 
     @staticmethod
     def warn(msg):
