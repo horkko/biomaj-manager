8675eedb48c05c56d6495ad392abf0b245a7b440
horkko <horkko@gmail.com>
2015-12-21 17:37:24 +0100

    Plugins for tests

---
+++ tests/plugins/myplugins.py
@@ -0,0 +1,32 @@
+__author__ = 'tuco'
+
+from biomajmanager.plugins import BMPlugin
+
+
+class Myplugins(BMPlugin):
+    """
+
+    """
+    def get_cfg_name(self):
+        return self.config.get(self.get_name(), 'myplugins.name')
+
+    def get_cfg_value(self):
+        return self.config.get(self.get_name(), 'myplugins.value')
+
+    def get_value(self):
+        return 1
+
+    def get_string(self):
+        return "test"
+
+    def get_false(self):
+        return False
+
+    def get_true(self):
+        return True
+
+    def get_none(self):
+        return None
+
+    def get_exception(self):
+        return Exception()
