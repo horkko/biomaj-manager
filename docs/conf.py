879bc40faec35e34b75a39344c5ed4db10204a71
horkko <horkko@gmail.com>
2016-04-02 14:43:54 +0200

    Updated conf.py to avoid readthedocs to fail at compile time

--- docs/conf.py
+++ docs/conf.py
@@ -14,16 +14,11 @@
 
 import sys
 import os
-
-from mock import Mock as MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-            return Mock()
+import mock
 
 MOCK_MODULES = ['pycurl', 'pymongo', 'elasticsearch', 'drmaa']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
