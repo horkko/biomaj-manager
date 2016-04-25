520c6a5b2b6bb7f4a0a6e745747e7079e8068b02
horkko <horkko@gmail.com>
2016-04-25 11:50:21 +0200

    Release 1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 config = {
     'name': 'biomajmanager',
-    'version': '1.1.1',
+    'version': '1.1.2',
     'packages': find_packages(),
     'scripts': ['bin/biomaj-manager.py'],
     'url': 'https://github.com/horkko/biomaj-manager',
