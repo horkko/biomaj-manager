40d3b41930fe77915ef35b4a5c6198667e6c51d2
horkko <horkko@gmail.com>
2016-08-24 16:10:33 +0200

    Try to install mongodb repo

--- tests/gitlab-tests.sh
+++ tests/gitlab-tests.sh
@@ -1,8 +1,22 @@
 #! /bin/sh
 
+cat <<EOF > /etc/yum.repos.d/mongodb-org-3.2.repo
+[mongodb-org-3.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc
+EOF
+
+yum clean all
+
+yum install -y mongodb-org
+su - service mongod start
+
 pip install -r ../requirements.txt
 pip install pymongo==3.2
 pip install nose
 
 nosetests --with-coverage --cover-package=biomajmanager
-
+exit 0
