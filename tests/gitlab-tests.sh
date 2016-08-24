6c11731dcc474ddf6f97b56e7f82598cd29b4030
horkko <horkko@gmail.com>
2016-08-24 17:28:08 +0200

    Try to also import requirements.txt in Docker env

--- tests/gitlab-tests.sh
+++ tests/gitlab-tests.sh
@@ -1,23 +1,13 @@
 #! /bin/sh
 
-#cat <<EOF > /etc/yum.repos.d/mongodb-org-3.2.repo
-#[mongodb-org-3.2]
-#name=MongoDB Repository
-#baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/3.2/x86_64
-#gpgcheck=1
-#enabled=1
-#gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc
-#EOF
 
 yum clean all
 yum install -y epel-release
-#yum install -y systemd
-#yum install -y mongodb-org
 yum install -y python-pip
 yum install -y python-nose
 #service mongod start
 
-pip install -r ../requirements.txt
+pip install requirements.txt
 pip install pymongo==3.2
 pip install nose
 
