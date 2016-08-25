6732f6029e6d098455ff8ddc49c3d8018565cbb9
horkko <horkko@gmail.com>
2016-08-25 10:03:06 +0200

    Moved all installation (yum and pip) into Dockerfile

--- tests/gitlab-tests.sh
+++ tests/gitlab-tests.sh
@@ -1,18 +1,22 @@
 #! /bin/sh
 
-
-yum clean all
-yum install -y epel-release
-yum install -y make gcc
-yum install -y python-pip
-yum install -y python-nose
-yum install -y git
-
-pip install humanfriendly
-pip install pymongo==3.2
-pip install biomaj
-pip install Jinja2
-pip install Yapsy
-pip install git+https://github.com/svpino/rfeed#egg=rfeed
-
+#yum clean all
+#yum install -y epel-release
+## Needed to compile some python packages
+#yum install -y make gcc
+## Required to install python packages
+#yum install -y python-pip
+## Required to run python tests
+#yum install -y python-nose
+## Required to install python package rfeed
+#yum install -y git
+#
+#pip install humanfriendly
+#pip install pymongo==3.2
+#pip install biomaj
+#pip install Jinja2
+#pip install Yapsy
+#pip install git+https://github.com/svpino/rfeed#egg=rfeed
+here=`pwd`
+echo "We are here $here"
 nosetests 
