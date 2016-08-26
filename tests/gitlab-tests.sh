84febc916f020fc482eb1b651979ddcce77e3e5d
horkko <horkko@gmail.com>
2016-08-26 09:12:14 +0200

    Try to find global.properties location

--- tests/gitlab-tests.sh
+++ tests/gitlab-tests.sh
@@ -23,6 +23,9 @@ pip install git+https://github.com/svpino/rfeed#egg=rfeed
 
 # Test mongo connection
 mongo --eval "db.serverStatus()" mongo/test || exit 1
-
+here=`pwd`
+echo "Here we are $pwd"
+#echo "Config file is $BIOMAJ_CONF"
 # Run tests for biomaj-manager
-BIOMAJ_CONF=global-docker.properties nosetests
+#BIOMAJ_CONF=global-docker.properties nosetests
+/bin/false
