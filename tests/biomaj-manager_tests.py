5f4fd2d2bc4590463a8aafa993992605307bbfbb
horkko <horkko@gmail.com>
2016-08-29 10:44:52 +0200

    Try to see what wrong with permissions links.py

--- tests/biomaj-manager_tests.py
+++ tests/biomaj-manager_tests.py
@@ -702,8 +702,8 @@ class TestBiomajManagerLinks(unittest.TestCase):
         """Check the method throws exception"""
         links = Links(manager=self.utils.manager)
         links.manager.set_verbose(False)
-        #os.chmod(self.utils.prod_dir, 0o000)
-        os.removedirs(self.utils.prod_dir)
+        os.chmod(self.utils.test_dir, 0o000)
+        #os.removedirs(self.utils.prod_dir)
         with self.assertRaises(SystemExit):
             links._clone_structure(source='golden', target='index', remove_ext=True)
         #os.chmod(self.utils.prod_dir, 0o777)
@@ -870,8 +870,8 @@ class TestBiomajManagerLinks(unittest.TestCase):
         """Check method throws when permissions denied to create dir"""
         link = Links(manager=self.utils.manager)
         link.manager.set_verbose(True)
-        #os.chmod(self.utils.prod_dir, 0o000)
-        os.removedirs(self.utils.prod_dir)
+        os.chmod(self.utils.test_dir, 0o000)
+        #os.removedirs(self.utils.prod_dir)
         with self.assertRaises(SystemExit):
             link._prepare_links(source='uncompressed', target='link_test')
         #os.chmod(self.utils.prod_dir, 0o777)
