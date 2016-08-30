1b3b0b9a41de1d9fcf23ae79318bfb16ed8a69fa
horkko <horkko@gmail.com>
2016-08-30 12:22:00 +0200

    Install mongo localy

--- tests/gitlab-tests.sh
+++ tests/gitlab-tests.sh
@@ -13,7 +13,9 @@ sudo yum install -y python-devel python-pip python-nose python-jinja2
 
 # Start mongodb server
 sudo mkdir -p /data/db
-sudo mongod --dbpath /data/db --fork
+sudo mongod --dbpath /data/db --logpath /var/log/mongodb.log --fork
+# Check mongodb connection
+mongo --eval "db.serverStatus()" localhost:27017/bm_db_test || exit 1
 
 ## Install some required packages
 sudo pip install humanfriendly
@@ -24,9 +26,6 @@ sudo pip install git+https://github.com/svpino/rfeed#egg=rfeed
 
 # Run tests for biomaj-manager with DOCKER tests
 export MONGO_URI="mongodb://localhost:27017/bm_db_test"
-
-# Check mongodb connection
-mongo --eval "db.serverStatus()" localhost:27017/bm_db_test || exit 1
 
 # Split tests
 for attr in 'utils' 'links' 'decorators' 'manager' 'plugins' 'writer'; do
