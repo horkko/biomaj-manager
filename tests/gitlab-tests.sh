9db04c5e8ca72b3761df8089b1d9c60496de682b
horkko <horkko@gmail.com>
2016-08-30 15:58:05 +0200

    Try to run tests

--- tests/gitlab-tests.sh
+++ tests/gitlab-tests.sh
@@ -18,24 +18,22 @@ sudo mongod --dbpath /data/db --logpath /var/log/mongodb.log --fork
 mongo --eval "db.serverStatus()" localhost:27017/bm_db_test || exit 1
 
 ## Install some required packages
-sudo pip install humanfriendly
-sudo pip install pymongo==3.2
+sudo pip install humanfriendly Yapsy pymongo==3.2
 sudo pip install --egg biomaj
-sudo pip install Yapsy
 sudo pip install git+https://github.com/svpino/rfeed#egg=rfeed
 
 # Run tests for biomaj-manager with DOCKER tests
 export MONGO_URI="mongodb://localhost:27017/bm_db_test"
+export LOGNAME='biomaj'
 
 cd /builds || { echo "Can't cd to /builds" && exit 1; }
 sudo python setup.py -q install || { echo "Install failed" && exit 1; }
 
 # Split tests
-#for attr in 'utils' 'links' 'decorators' 'manager' 'plugins' 'writer'; do
-#    echo "[BIOAMJ_MANAGER_TESTS] * Running test $attr "
-#    nosetests -w /builds/tests -a '$attr' || { echo "[BIOAMJ_MANAGER_TESTS] * $attr failed" && exit 1; }
-#    echo "[BIOMAJ_MANAGER_TESTS] * $attr OK"
-#done
-nosetests -x
+for attr in 'utils' 'links' 'decorators' 'manager' 'plugins' 'writer'; do
+    echo "[BIOAMJ_MANAGER_TESTS] * Running test $attr "
+    nosetests -a "$attr" --with-coverage --cover-package=biomajmanager || { echo "[BIOAMJ_MANAGER_TESTS] * $attr tests failed" && exit 1; }
+    echo "[BIOMAJ_MANAGER_TESTS] * $attr OK"
+done
 
 exit 0
