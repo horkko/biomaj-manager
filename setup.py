ab2fb234b1f03d8788b9ba72e1b34a299230b789
horkko <horkko@gmail.com>
2016-08-31 09:54:01 +0200

    Release 1.1.6: Bug fixes and tests with gitlab available

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 config = {
     'name': 'biomajmanager',
-    'version': '1.1.5',
+    'version': '1.1.6',
     'packages': find_packages(),
     'scripts': ['bin/biomaj-manager.py'],
     'url': 'https://github.com/horkko/biomaj-manager',
