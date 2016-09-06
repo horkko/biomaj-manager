2535a0c5203d8c5aa96f152f15b60b569436c398
horkko <horkko@gmail.com>
2016-09-06 14:48:09 +0200

    Do not install using setup.py

--- tests/gitlab-tests.sh
+++ tests/gitlab-tests.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
-# Install BioMAJ Manager to test it later on
-#sudo python setup.py -q install || { echo "Install failed" && exit 1; }
+# Set env variable to use right MongoDB URI string
 export BIOMAJ_MANAGER_DOCKER_CONF="/builds/tuco/biomaj-manager/tests/global-docker.properties"
+nosetests -q -x || { echo "[BIOAMJ_MANAGER_TESTS] Tests failed" && exit 1; }
 
 # Split tests
-for attr in 'utils' 'links' 'decorators' 'manager' 'plugins' 'writer'; do
-    echo "[BIOAMJ_MANAGER_TESTS] * Running test $attr "
-    nosetests -a "$attr" -q -x || { echo "[BIOAMJ_MANAGER_TESTS] * $attr tests failed" && exit 1; }
-    echo "[BIOMAJ_MANAGER_TESTS] * $attr OK"
-done
+#for attr in 'utils' 'links' 'decorators' 'manager' 'plugins' 'writer'; do
+#    echo "[BIOAMJ_MANAGER_TESTS] * Running test $attr "
+#    nosetests -a "$attr" -q -x || { echo "[BIOAMJ_MANAGER_TESTS] * $attr tests failed" && exit 1; }
+#    echo "[BIOMAJ_MANAGER_TESTS] * $attr OK"
+#done
 
 exit 0
