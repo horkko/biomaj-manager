86bc96ec110eda663f6b4c7807a9fecc88751f2f
horkko <horkko@gmail.com>
2016-09-06 11:40:06 +0200

    First Docker image build and try to test

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,25 @@
 FROM centos:centos7
 MAINTAINER Emmanuel Quevillon <tuco@pasteur.fr>
 
-RUN yum -y update
 RUN yum clean all
 
 RUN yum install -y epel-release
-RUN yum install -y sudo
+RUN yum install -y make gcc mongodb-server mongodb git python-devel python-pip python-nose python-jinja2 && yum clean all
+
+# Start mongodb server
+#RUN mkdir -p /data/db
+#RUN mongod --dbpath /data/db --logpath /var/log/mongodb.log --fork
+
+## Install some required packages
+RUN pip install -U pip && pip install humanfriendly Yapsy pymongo==3.2 'git+https://github.com/svpino/rfeed#egg=rfeed' && pip install --egg biomaj
 
 # Create new user/group biomaj
 RUN groupadd -r biomaj && \
     useradd -m -d /home/biomaj -r -g biomaj biomaj
 
-ADD ./docker-sudo /etc/sudoers.d/docker
-RUN chmod 0600 /etc/sudoers.d/docker
-
-RUN echo "Defaults:biomaj        !requiretty" >> /etc/sudoers
+# Run tests for biomaj-manager with DOCKER tests
+ENV LOGNAME='biomaj'
+ENV USER='biomaj'
 
 # Run all as user biomaj
 USER biomaj
--- tests/gitlab-tests.sh
+++ tests/gitlab-tests.sh
@@ -1,31 +1,5 @@
 #! /bin/sh
 
-sudo yum clean all
-sudo yum install -y epel-release
-## Needed to compile some python packages
-sudo yum install -y make gcc
-# Test mongodb respond, install client
-sudo yum install -y mongodb-server mongodb
-## Required to install python package rfeed
-sudo yum install -y git
-## Install Python packages
-sudo yum install -y python-devel python-pip python-nose python-jinja2
-
-# Start mongodb server
-sudo mkdir -p /data/db
-sudo mongod --dbpath /data/db --logpath /var/log/mongodb.log --fork
-# Check mongodb connection
-mongo --eval "db.serverStatus()" localhost:27017/bm_db_test || exit 1
-
-## Install some required packages
-sudo pip install humanfriendly Yapsy pymongo==3.2
-sudo pip install --egg biomaj
-sudo pip install git+https://github.com/svpino/rfeed#egg=rfeed
-
-# Run tests for biomaj-manager with DOCKER tests
-export LOGNAME='biomaj'
-export USER='biomaj'
-
 # Install BioMAJ Manager to test it later on
 cd /builds || { echo "Can't cd to /builds" && exit 1; }
 sudo python setup.py -q install || { echo "Install failed" && exit 1; }
