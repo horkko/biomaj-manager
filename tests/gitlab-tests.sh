e005a4cececf82c74185e5764764af53e53962d7
horkko <horkko@gmail.com>
2016-09-06 15:12:15 +0200

    Minimized commands in gitlab-tests.sh

--- tests/gitlab-tests.sh
+++ tests/gitlab-tests.sh
@@ -4,11 +4,4 @@
 export BIOMAJ_MANAGER_DOCKER_CONF="/builds/tuco/biomaj-manager/tests/global-docker.properties"
 nosetests -q -x || { echo "[BIOAMJ_MANAGER_TESTS] Tests failed" && exit 1; }
 
-# Split tests
-#for attr in 'utils' 'links' 'decorators' 'manager' 'plugins' 'writer'; do
-#    echo "[BIOAMJ_MANAGER_TESTS] * Running test $attr "
-#    nosetests -a "$attr" -q -x || { echo "[BIOAMJ_MANAGER_TESTS] * $attr tests failed" && exit 1; }
-#    echo "[BIOMAJ_MANAGER_TESTS] * $attr OK"
-#done
-
 exit 0
